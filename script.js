4952c07622f2314e0038304b163ee3fd776e55ea
MJKDigiHub <MJKDigiHub@gmail.com>
2024-11-11 17:35:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// Placeholder for future functionality
-console.log("Welcome to MJK DigiHub!");
+// JavaScript functions for interactivity
 
+function handleWhatsAppClick() {
+    alert("Opening WhatsApp to contact MJK DigiHub!");
+    window.location.href = "https://wa.me/919072906193";
+}
+
+function handleCallClick() {
+    alert("Calling MJK DigiHub at +91 9072906193!");
+    window.location.href = "tel:+919072906193";
+}
+
+function handleEmailClick() {
+    alert("Opening email to contact MJK DigiHub!");
+    window.location.href = "mailto:MJKDigiHub@gmail.com";
+}
